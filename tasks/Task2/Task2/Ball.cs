779d4b3d2307809986c922734a1bb65b760f646e
Michael Mueller <ic15b028@TECHNIKUM-WIEN.AT>
2016-02-16 20:09:54 +0100

    add Task2

---
+++ tasks/Task2/Task2/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Ball
+    {
+        public int preis;
+        public int size;
+        private string color;
+
+        public Ball()
+        {
+            preis = 1;
+            size = 5;
+        }
+        public Ball(int preis)
+        {
+            this.preis = preis;
+        }
+
+        public void SetColor(string color)
+        {
+            this.color = color;
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Ball();
+
+            Console.WriteLine("Size of ball is " + a.size);
+
+            a.SetColor("black");
+
+            Console.WriteLine("Color of ball is " + a.GetColor());
+
+        }
+    }
+}
